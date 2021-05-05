560c09f61a40e31134effcaee148f49f847b3d47
Pau Simarro Herrera <pau.simarro@gmail.com>
2021-05-05 15:25:44 +0200

    code correction

--- nivell1/script.js
+++ nivell1/script.js
@@ -1,63 +1,18 @@
-document.getElementById('divRating').onclick = changeColor;   
-// document.body.onclick = setDefault;
 
-function changeColor() {
-    
-    return false;
-}   
+function addRates(value) {
+    for (var i = 1; i <= 5; i++) {
+        document.getElementById("divRating" + i).classList.remove("active");
+    }
+    for (var i = 1; i <= value; i++) {
+        document.getElementById("divRating" + i).classList.add("active");
+    }
+}
 
-
-var body = document.body;
-var divRating1 = document.getElementById('divRating1');
-var divRating2 = document.getElementById('divRating2');
-var divRating3 = document.getElementById('divRating3');
-var divRating4 = document.getElementById('divRating4');
-var divRating5 = document.getElementById('divRating5');
-
-body.addEventListener("click", function () {
-    divRating1.style.color = "black";
-    divRating2.style.color = "black";
-    divRating3.style.color = "black";
-    divRating4.style.color = "black";
-    divRating5.style.color = "black";
+document.body.addEventListener("click", (event) => {
+    for (var i = 1; i <= 5; i++) {
+        document.getElementById("divRating" + i).classList.remove("active");
+    }
 }, false);
-divRating1.addEventListener("click", function (ev) {
-    divRating1.style.color="lightblue";
-    divRating2.style.color = "black";
-    divRating3.style.color = "black";
-    divRating4.style.color = "black";
-    divRating5.style.color = "black";
-    ev.stopPropagation(); //this is important! If removed, you'll get both alerts
-}, false);
-divRating2.addEventListener("click", function (ev) {
-    divRating1.style.color="lightblue";
-    divRating2.style.color="lightblue";
-    divRating3.style.color = "black";
-    divRating4.style.color = "black";
-    divRating5.style.color = "black";
-    ev.stopPropagation(); //this is important! If removed, you'll get both alerts
-}, false);
-divRating3.addEventListener("click", function (ev) {
-    divRating1.style.color="lightblue";
-    divRating2.style.color="lightblue";
-    divRating3.style.color="lightblue";
-    divRating4.style.color = "black";
-    divRating5.style.color = "black";
-    ev.stopPropagation(); //this is important! If removed, you'll get both alerts
-}, false);
-divRating4.addEventListener("click", function (ev) {
-    divRating1.style.color="lightblue";
-    divRating2.style.color="lightblue";
-    divRating3.style.color="lightblue";
-    divRating4.style.color="lightblue";
-    divRating5.style.color = "black";
-    ev.stopPropagation(); //this is important! If removed, you'll get both alerts
-}, false);
-divRating5.addEventListener("click", function (ev) {
-    divRating1.style.color="lightblue";
-    divRating2.style.color="lightblue";
-    divRating3.style.color="lightblue";
-    divRating4.style.color="lightblue";
-    divRating5.style.color="lightblue"
-    ev.stopPropagation(); //this is important! If removed, you'll get both alerts
+document.getElementById("divRating").addEventListener("click", function (ev) {
+    ev.stopPropagation();
 }, false);
